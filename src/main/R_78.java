b801583a33cf26e1d7afaca3931ecf7906ac6010
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-05-23 08:18:32 +0900

    D-3
    v-1.1
    ---
    1. LNG price
    
    	==> done

--- src/main/R_78.java
+++ src/main/R_78.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -17,10 +18,199 @@ public class R_78 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ops();
+		ops_V_1_1();
+		
+//		ops();
+
+//		ops_residue();
 		
 	}
 
+	/*******************************
+	 * for: data in http://ecodb.net/pcp/imf_group_ngas.html
+	 *******************************/
+	private static void ops_V_1_1() {
+		// TODO Auto-generated method stub
+
+		///////////////////////////////////
+		//
+		// vars
+		//
+		///////////////////////////////////
+		String dpath = "data";
+		
+		String fname_Src_trunk = "R.78.v_1_1.LNG-price";
+		
+		String ext = ".txt";
+		
+		String fname_Dst_trunk = fname_Src_trunk + "PROCESSED";
+		
+		String fpath_Src = dpath + "/" + fname_Src_trunk + ext;
+
+		String fpath_Dst = dpath + "/" + fname_Dst_trunk + ext;
+
+		///////////////////////////////////
+		//
+		// processes
+		//
+		///////////////////////////////////
+		
+		try {
+			
+			BufferedReader br = new BufferedReader(new FileReader(fpath_Src));
+			
+//			String line = br.readLine();		// 1st line (header)
+
+			String line = null;
+			
+			int counter = 0, tmp_i, res;
+			
+			String[] list = new String[4];
+			
+			// init
+			for(int i = 0; i < 4; i ++) list[i] = "";
+			
+			///////////////////////////////////
+			//
+			// first 4 lines
+			//
+			///////////////////////////////////
+			for (int i = 0; i < 4; i++) {
+				
+				list[i] += br.readLine();
+				
+			}
+			
+			///////////////////////////////////
+			//
+			// lines: 2nd ~
+			//
+			///////////////////////////////////
+			line = br.readLine();
+			
+			String[] splits = null;
+			
+			while (line != null) {
+				
+				splits = line.split("\t");
+				
+//				list[counter] += "\t" + StringUtils.join(
+//						Arrays.copyOfRange(splits, 1, splits.length), "\t");
+//				list[counter] += "\t" + line;
+				
+				if (counter % 4 == 3) {
+//					if (counter >= 3) {
+//					if ((counter % 3) == 0) {
+//					if (tmp_i == 0) {
+					
+//					splits = line.split("\t");
+					
+					list[counter] += "\t" + StringUtils.join(
+										Arrays.copyOfRange(splits, 1, splits.length), "\t");
+//					list[counter] += "\t" + StringUtils.join(splits, "\t");
+//					list[counter] += "\t" + line;
+					
+					counter = 0;
+					
+					String msg;
+					msg = String.format(Locale.JAPAN, "[%s : %d] line => %s", Thread
+							.currentThread().getStackTrace()[1].getFileName(),
+							Thread.currentThread().getStackTrace()[1]
+									.getLineNumber(), 
+							StringUtils.join(
+									Arrays.copyOfRange(splits, 1, splits.length), "\t"));
+
+					System.out.println(msg);
+					
+					
+//					tokens = new String[4];
+					
+				} else {
+					
+					list[counter] += "\t" + StringUtils.join(
+							Arrays.copyOfRange(splits, 1, splits.length), "\t");
+					
+					counter ++;
+					
+				}
+				
+				// new line
+				line = br.readLine();
+				
+			}
+			
+			br.close();
+			
+			///////////////////////////////////
+			//
+			// data => save
+			//
+			///////////////////////////////////
+//			String[] tmp_ary = null;
+//			
+//			String tmp_s = null;
+//			
+			BufferedWriter bw = new BufferedWriter(new FileWriter(fpath_Dst));
+			
+			for (int i = 0; i < list.length; i++) {
+				
+//				tmp_ary = list.get(i);
+				
+//				tmp_s = StringUtils.join(tmp_ary, "\t");
+				
+				bw.write(list[i]);
+				
+				bw.write("\n");
+				
+			}
+			
+			bw.close();
+			
+			String msg;
+			msg = String.format(Locale.JAPAN, "[%s : %d] bw => closed", Thread
+					.currentThread().getStackTrace()[1].getFileName(), Thread
+					.currentThread().getStackTrace()[1].getLineNumber());
+
+			System.out.println(msg);
+			
+		} catch (FileNotFoundException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}//try
+
+	}//private static void ops_V_1_1()
+
+	private static void ops_residue() {
+		// TODO Auto-generated method stub
+
+		int a = 0, b = 4;
+		
+		String msg;
+		
+		for (int i = 0; i < 10; i++) {
+			
+			msg = String.format(Locale.JAPAN, "[%s : %d] i = %d, b = %d => i %% b = %d", Thread
+					.currentThread().getStackTrace()[1].getFileName(), Thread
+					.currentThread().getStackTrace()[1].getLineNumber(), i, b, i % b);
+			
+			System.out.println(msg);
+			
+		}
+		
+		
+		
+	}
+
+	/*******************************
+	 * R.78.new_energy.txt => R.78.new_energy.PROCESSED.txt
+	 *******************************/
 	private static void ops() {
 		// TODO Auto-generated method stub
 		
